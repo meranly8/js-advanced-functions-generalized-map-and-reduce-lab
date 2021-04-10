4af544f8fbbc4c7b28339da832dd4e3eb679d993
Michelle Ranly <meranly8@gmail.com>
2021-04-09 21:48:04 -0500

    commit tree 781f5528f38ee850b72d5df2d7548171c484aaef

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(arry, ftn) {
+    let newArry = []
+    
+    for (let i = 0; i < arry.length; i++){
+        newArry.push(ftn(arry[i]))
+    }
+    return newArry
+}
+
+function reduce(arry, ftn, start) {
+    let total = (!!start) ? start : arry[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < arry.length; i++){
+        total = ftn(arry[i], total)
+    }
+    return total
+}
